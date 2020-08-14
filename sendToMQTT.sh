ec6e8ffad49b43f608cdaa62ea14c353216562cf
Zenedith <github@zenedith.mailgun.org>
2020-08-15 00:43:00 +0200

    fix topic name

--- sendToMQTT.sh
+++ sendToMQTT.sh
@@ -5,4 +5,4 @@
 
 #echo "sendToMQTT: $1 sensor: $2 temp: $3 hum: $4 batt volt: $5 battery level: $6"
 
-mosquitto_pub -h $MQTT_HOST -t "$MQTT_TOPIC_PREFIX/$2/temp" -u $MQTT_USERNAME -P $MQTT_PASSWD -m "{\"temperature\": \"$3\", \"humidity\": \"$4\", \"batt\": \"$6\"}"
+mosquitto_pub -h $MQTT_HOST -t "$MQTT_TOPIC_PREFIX/$2" -u $MQTT_USERNAME -P $MQTT_PASSWD -m "{\"temperature\": \"$3\", \"humidity\": \"$4\", \"batt\": \"$6\"}"
